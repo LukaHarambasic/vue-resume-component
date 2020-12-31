fdca568a7186c3f5578afc4aa452c03af2ff8e11
Luka Harambasic <luka@harambasic.de>
2020-12-31 01:58:30 +0100

    Add mock content and assets for development

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,36 @@
 <template>
+  <resume
+    :settings="settings"
+    :resume="resume"
+  />
 </template>
 
 <script>
+import Resume from '@/components/Resume'
+
+// Mock Data - this has to be provided by the user
+import MOCK_SETTINGS from '@/mock/settings.private.json'
+import MOCK_RESUME from '@/mock/resume.private.json'
+
 export default {
   name: 'App',
+  components: {
+    Resume
+  },
+  data () {
+    return {
+      settings: MOCK_SETTINGS,
+      resume: MOCK_RESUME
+    }
+  }
 }
 </script>
 
-<style lang="sass" scoped>
+<style lang="sass">
+body
+  background: #e2e2e2
+#app
+  display: grid
+  place-items: center
+  margin: 1rem
 </style>
