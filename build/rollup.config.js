2d8472a55d2e113a19491e349de4142c74c8589f
Luka Harambasic <luka@harambasic.de>
2021-01-02 13:56:54 +0100

    Cleaning mock, rollup config & package json

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -1,8 +1,9 @@
-import commonjs from '@rollup/plugin-commonjs'; // Convert CommonJS modules to ES6
-import vue from 'rollup-plugin-vue'; // Handle .vue SFC files
-import buble from '@rollup/plugin-buble'; // Transpile/polyfill with reasonable browser support
+import commonjs from '@rollup/plugin-commonjs'
+import vue from 'rollup-plugin-vue'
+import buble from '@rollup/plugin-buble'
+
 export default {
-  input: 'src/wrapper.js', // Path relative to package.json
+  input: 'src/wrapper.js',
   output: {
     name: 'VueResume',
     exports: 'named',
@@ -10,9 +11,9 @@ export default {
   plugins: [
     commonjs(),
     vue({
-      css: true, // Dynamically inject css as a <style> tag
-      compileTemplate: true, // Explicitly convert template to render function
+      css: true,
+      compileTemplate: true,
     }),
-    buble(), // Transpile to ES5
+    buble(),
   ],
 };
