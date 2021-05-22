adf5be1c1c35c37d6bbdd6e1c262035ff6109ba5
Luka Harambasic <luka@harambasic.de>
2021-05-22 23:38:58 +0200

    vite build setup

--- vite.config.js
+++ vite.config.js
@@ -9,5 +9,25 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, '/src'),
     },
+  },
+
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/wrapper.js'),
+      name: 'VueResume',
+      formats: ['es', 'cjs', 'umd', 'iife']
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['vue', 'sass'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    }
   }
 })
